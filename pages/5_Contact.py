579dcf6b8dfc755140e42ffb7a6ab93437a4e094
asidaryu <148434733+asidaryu@users.noreply.github.com>
2023-11-07 09:33:20 +0700

    Update 5_Contact.py

--- pages/5_Contact.py
+++ pages/5_Contact.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
+import plotly.express as px
 import streamlit as st
+import pandas as pd
 
 # Menambahkan title dan icon pada page
 st.set_page_config(
-    page_title="Contact",
-    page_icon="🐼"
+    page_title="Shopping Trends",
+    page_icon="🛍️"
 )
+
 # Menambahkan gambar di sidebar
 st.sidebar.markdown(
     '<div style="display: flex; justify-content: center; position: fixed; top: 0; width: 23%; height: 12%; margin-top: 1%;">'
@@ -12,84 +16,66 @@ st.sidebar.markdown(
     '</div>',
     unsafe_allow_html=True
 )
-# Menambahkan gambar di samping Address
-st.markdown(
-        '<div style="display: flex; justify-content: left; position: absolute; top: 0; width: 7%; height: 10%; margin-top: 42%;">'
-        '<img src="https://i.pinimg.com/564x/2b/3f/59/2b3f5903e6ef99075ab2f2fee8c11763.jpg" style="object-fit: cover; width: 40px; height: 40px; border-radius: 50%; overflow: hidden;">'
-        '</div>',
-        unsafe_allow_html=True
-    )
-# Menambahkan gambar di samping Phone
-st.markdown(
-        '<div style="display: flex; justify-content: left; position: absolute; top: 0; width: 7%; height: 10%; margin-top: 55%;">'
-        '<img src="https://i.pinimg.com/564x/8d/e5/50/8de5507d6f51f9c8b64d90c175c0b1e5.jpg" style="object-fit: cover; width: 40px; height: 40px; border-radius: 50%; overflow: hidden;">'
-        '</div>',
-        unsafe_allow_html=True
-    )
-# Menambahkan gambar di samping Email
-st.markdown(
-        '<div style="display: flex; justify-content: left; position: absolute; top: 0; width: 7%; height: 10%; margin-top: 64%;">'
-        '<img src="https://i.pinimg.com/564x/45/af/cb/45afcb072e032c54b0dfc739e67d25ab.jpg" style="object-fit: cover; width: 40px; height: 40px; border-radius: 50%; overflow: hidden;">'
-        '</div>',
-        unsafe_allow_html=True
-    )
 
-def main():
-    # Membuat cover pada menu Contact
-    page_bg_img = """
-    <style>
-    [data-testid="stAppViewContainer"] {
-    background-image: url("https://wallpapercave.com/wp/udSKWo6.jpg");
-    background-size: cover;
-    }
-    </style>
-    """
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    # Mengatur font-size, font-family, dan text berada di tengah
+# Filter berdasarkan kategori
+category = st.sidebar.selectbox('Pilih Category', ['Clothing', 'Footwear', 'Accessories'])
+
+# Filter berdasarkan ukuran
+size = st.sidebar.radio(
+    "Pilih Size:",
+    ('S', 'M', 'L', 'XL')
+)
+
+# Judul pada page Data
+st.markdown(
+    '<div style="font-size: 40px; font-family: timesnewroman; margin-bottom: 50px;">Shopping Trends</div>',
+    unsafe_allow_html=True
+)
+st.write('Gender: Male')
+
+# Data berdasarkan Pendidikan Tertinggi Yang Ditamatkan dan Tahunnya
+data = {
+     "Item Purchased": ["Blouse", "Sweater", "Jeans", "Sandals", "Sneakers", "Shirt", "Coat", "Handbag"],
+     "Category Clothing": ["Blouse", "Sweater", "Jeans", "Shirt", "Short", "Dress", "Dress", "Pants"],
+     "Category Footwear": ["Sandals", "Sneakers", "Boots", "Shoes", "Flat Shoes", "Ballet Flats", "Galoshes", "Oxfords"],
+     "Category Accessories": ["Handbag", "Sunglasses", "Jewelry", "Scarf", "Hat", "Backpack", "Belt", "Gloves"],
+     "Purchase Amount (USD)": [53, 64, 73, 90, 49, 20, 85, 34],
+     "Purchase Amount (USD) Size S": [73, 36, 45, 21, 94, 43, 32, 73],
+     "Purchase Amount (USD) Size M": [90, 81, 69, 94, 36, 91, 95, 92],
+     "Purchase Amount (USD) Size L": [53, 53, 43, 79, 46, 53, 41, 100],
+     "Purchase Amount (USD) Size XL": [38, 21, 59, 73, 72, 83, 67, 40],
+}
+# Menambahkan tabs
+lingkaran, batang, tabel = st.tabs(["GRAFIK LINGKARAN", "GRAFIK BATANG", "TABEL"])
+
+colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0','#ffb3e6', '#c2f0c2', '#ff6666']
+
+with lingkaran:
+    # Data
+    labels = data[f"Category {category}"]
+    sizes = data[f"Purchase Amount (USD) Size {size}"]
+
+    # Explode untuk bagian yang ditekankan
+    explode = (0, 0, 0, 0, 0.1, 0, 0.1, 0.2)
+
+    fig1, ax1 = plt.subplots()
+
+    # Menambahkan labels, warna, dan explode
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90, colors=colors, explode=explode)
+    ax1.axis('equal')
+
+    # Menampilkan grafik
+    st.pyplot(fig1)
+
+with batang:
+    fig = px.bar(data, x=f"Category {category}", y=f"Purchase Amount (USD) Size {size}", color=colors)
+    st.plotly_chart(fig)
+
+with tabel:
+    df = pd.DataFrame(data)
     st.markdown(
-        '<div style="text-align: center; font-size: 60px; font-family: timesnewroman;">Contact Us</div>'
-        '<div style="text-align: center; font-size: 19px; font-family: timesnewroman;">Silahkan hubungi kami jika anda mengalami kendala atau masalahan terkait informasi atau hal lainnya saat mengunjungi dashboard kami</div>',
+        '<div style="margin-top: 20px;"></div>',
         unsafe_allow_html=True
     )
-    # Membuat 2 column
-    col1, col2 = st.columns(2)
-
-    # Menambahkan konten ke setiap kolom
-    with col1: # Column 1 berisi data tentang alamat, phone, dan email
-        st.markdown(
-            '<div style="font-size: 20px; font-family: timesnewroman; color: cyan; margin-top: 70px; margin-left: 50px;">Address</div>'
-            '<div style="font-size: 15px; font-family: timesnewroman; margin-left: 50px;">4671 Sugar Camp Road,<br>Owatonna, Minnesota</div>',
-            unsafe_allow_html=True
-        )
-        st.markdown(
-            '<div style="font-size: 20px; font-family: timesnewroman; color: cyan; margin-top: 25px; margin-left: 50px;">Phone</div>'
-            '<div style="font-size: 15px; font-family: timesnewroman; margin-left: 50px;">(+62) 89-668-276-136</div>',
-            unsafe_allow_html=True
-        )
-        st.markdown(
-            '<div style="font-size: 20px; font-family: timesnewroman; color: cyan; margin-top: 25px; margin-left: 50px;">Email</div>'
-            '<div style="font-size: 15px; font-family: timesnewroman; margin-left: 50px;">pengangguran_163@gmail.com</div>',
-            unsafe_allow_html=True
-        )
-
-    with col2: # Column 2 untuk mengirimkan pesan jika mengalami kendala
-        st.markdown(
-            '<div style="font-size: 20px; font-family: timesnewroman; margin-top: 70px; color: plum;">Send Message</div>',
-            unsafe_allow_html=True
-        )
-        name = st.text_input("Name")
-        email = st.text_input("Email")
-        description = st.text_area("Description")
-
-        if st.button("Send"):
-            if name and email and description:  # Memastikan semua input terisi
-                st.success("Your message has been sent!")
-                st.write("Name:", name)
-                st.write("Email:", email)
-                st.write("Description:", description)
-            else:
-                st.warning("Please fill in all fields.")
-
-if __name__ == '__main__':
-
-    main()
+    filtered_df = df[[f"Category {category}", f"Purchase Amount (USD) Size {size}"]]
+    st.write(filtered_df)
